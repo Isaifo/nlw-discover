afca59e23f50c915a74bbf8354b14effe68ebf2e
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-11-09 22:24:20 -0300

    inserindo id's das perguntas

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,10 +25,13 @@ checkButtons.forEach(button => {
         event.preventDefault()
         const text = check ? "Marcar como lida" : "Excluir"
         const slug = check ? "check" : "delete"
+        const roomId = document.querySelector("#room-id").dataset.id
+        const questionId = event.target.dataset.id 
 
-        const roomId = document.querySelector("#room-id").dataset.id 
+
+
         const form = document.querySelector(".modal form")
-        form.setAttribute("action", `/room/${roomId}/:question/${slug}`)
+        form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
 
         modalTitle.innerHTML = `${text} esta pergunta`
         modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta`
