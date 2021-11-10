2266ba8d8290d598b217a4dd234fc12fe9c53d38
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-11-09 21:39:23 -0300

    renderizando rotas

--- src/route.js
+++ src/route.js
@@ -5,6 +5,7 @@ const route = express.Router() // route possui todas as funcionalidades de rotas
 route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/room/:room/:question/:action',(req,res) => res.render("exemplo", {req}))// criando variável para receber o conteúdo da url
 
 
 module.exports = route
