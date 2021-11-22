7312af0fbbdc0d62bf1dd4c029249d599164ed69
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-11-22 16:47:37 -0300

    primeiras configuracoes de database

--- src/route.js
+++ src/route.js
@@ -9,7 +9,7 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.get('/room/:room', (req, res) => res.render("room"))
 // Formato que o formulario de dentro da modal deva passar a informação
- route.post('/room/:room/:question/:action',QuestionController.index ) // passando o Question controller pela rota
-route.post('/room/create-room', RoomController.create )
+ route.post('/question/:room/:question/:action',QuestionController.index ) // passando o Question controller pela rota
+route.post('/create-room', RoomController.create )
 
 module.exports = route
