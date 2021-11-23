9ca40ebca283febeedabd5bf2c7627952d7cadfe
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-11-23 17:39:03 -0300

    inserindo as perguntas dinamicamente

--- src/route.js
+++ src/route.js
@@ -7,9 +7,12 @@ const route = express.Router() // route possui todas as funcionalidades de rotas
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
 
+route.post('/create-room', RoomController.create )
 route.get('/room/:room', RoomController.open)
+
+route.post('/question/create/:room', QuestionController.create)
 // Formato que o formulario de dentro da modal deva passar a informação
  route.post('/question/:room/:question/:action',QuestionController.index ) // passando o Question controller pela rota
-route.post('/create-room', RoomController.create )
+
 
 module.exports = route
