3622f03b531b3b6cfa56238c3645908780ae5d6b
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2021-11-24 11:54:19 -0300

    aperfeiçoando as rotas

--- src/route.js
+++ src/route.js
@@ -9,7 +9,7 @@ route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'
 
 route.post('/create-room', RoomController.create )
 route.get('/room/:room', RoomController.open) // lendo a rota
-route.post('/enterrom', RoomController.enter) //enviando a rota
+route.post('/enterroom', RoomController.enter) //enviando a rota
 
 route.post('/question/create/:room', QuestionController.create)
 // Formato que o formulario de dentro da modal deva passar a informação
